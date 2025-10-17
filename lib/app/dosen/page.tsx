0db04f0954621e835b1937c7b57eaedb5d55153c
Elsya Putri <nandaaelsya@gmail.com>
2025-10-17 21:21:07 +0700

    Delete lib directory

--- lib/app/dosen/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function DosenRootPage() {
-  // Mengarahkan (redirect) pengguna dari /dosen ke /dosen/dashboard
-  redirect('/dosen/dashboard');
-}
--- lib/app/dosen/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileForm } from '@/components/dosen/ProfileForm';
-
-export default function DosenProfilePage() {
-  return (
-    <div className="py-6"> 
-      <ProfileForm />
-    </div>
-  );
-}
