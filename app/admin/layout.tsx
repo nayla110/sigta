0c0f7f8f44880dbabd6dbec20358ebe02050b1c2
nayla110 <naylachindysagita08199@gmail.com>
2025-11-02 20:58:11 +0700

    Update Admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -19,8 +19,12 @@ export default function AdminLayout({
       {/* SIDEBAR */}
       <aside className="fixed top-0 left-0 h-full w-[250px] bg-[#1d4f91] text-white flex flex-col justify-between shadow-lg">
         <div>
-          <div className="py-6 text-center border-b border-white/20">
-            <h1 className="text-xl font-bold tracking-wide">Admin Panel</h1>
+         <div className="items-center justify-center">
+  <img
+    src="/icons/logo.png"
+    alt="logo"
+    className="w-60 h-25 object-contain"
+  />
           </div>
 
           <nav className="mt-6">
@@ -65,9 +69,8 @@ export default function AdminLayout({
           </nav>
         </div>
 
-        <div className="text-center mb-6 text-sm text-white/70">
-          <p>© 2025 Sistem Akademik</p>
-          <p className="hover:text-white transition">About us</p>
+         <div className="px-6 py-4 text-sm opacity-80 text-center">
+          <Link href="https://www.polibatam.ac.id/" >About us </Link>
         </div>
       </aside>
 
@@ -78,6 +81,7 @@ export default function AdminLayout({
           <h1 className="text-lg font-semibold text-gray-700">
             Selamat Datang Admin
           </h1>
+          
           <button className="bg-[#dc2626] hover:bg-[#b91c1c] text-white font-medium px-4 py-2 rounded-lg shadow-md transition duration-200">
             Keluar
           </button>
